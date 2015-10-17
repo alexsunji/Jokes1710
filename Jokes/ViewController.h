7d2cadb5f9a536e233ec4d19338eb0f47971116b
alexsunji <alexsunji@gmail.com>
2015-10-17 12:47:07 +0300

    Initial Commit

---
+++ Jokes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jokes
+//
+//  Created by Alex Sunji on 17.10.15.
+//  Copyright (c) 2015 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Jokes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Jokes
+//
+//  Created by Alex Sunji on 17.10.15.
+//  Copyright (c) 2015 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
